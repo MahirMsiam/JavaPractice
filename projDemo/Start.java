0a1e6af01bd300cdd9771fa29c06c493126ff90b
Mahir Mahmud Siam <mahirmahmudsiam@gmail.com>
2023-11-17 16:12:44 +0600

    Update Start.java

--- projDemo/Start.java
+++ projDemo/Start.java
@@ -1,10 +1,11 @@
-import javax.swing.*;  
-import java.awt.event.*; 
-public class Start{
-	
-	public static void main(String[] args) {  
-	 
-		 new Login();
-		
-	} 
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Start {
+
+	public static void main(String[] args) {
+
+		new Login();
+
+	}
 }
